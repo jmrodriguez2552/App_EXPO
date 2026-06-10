2187ddda8eedabd56e0adb0d842e23ced92a62ff
jmrodriguez2552 <jmrodriguez2552@gmail.com>
2026-06-10 18:14:53 +0200

    Primeros paso tutorial EXPO

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,15 +1,10 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
-import { useColorScheme } from 'react-native';
+import { Stack } from "expo-router";
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
+export default function RootLayout() {
+  return (<Stack >
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
-  );
+    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    
+  </Stack>);
+    
 }
